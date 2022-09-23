02dcbc7dc57a06f8cb7c096d1aaa216eb459079e
Yerlan Karakulov <smartdevkz@mail.ru>
2022-09-24 02:07:20 +0600

    fix Event comparison

--- resources/js/components/EventList.vue
+++ resources/js/components/EventList.vue
@@ -1,22 +1,37 @@
 <template>
-    <div>
-        <select @change="onCategoryChange($event)" v-model="selectedCategory">
-            <option value="0">Not selected</option>
-            <option v-for="item in categories" v-bind:value="{ id: item.id, text: item.name }">
-                {{ item.name }}
-            </option>
-        </select>
+    <div class="mb-3 row">
+        <label for="lstCategories" class="form-label">Категория</label>
+        <div class="col-sm-3">
+            <select id="lstCategories" @change="onCategoryChange($event)" v-model="selectedCategory"
+                class="form-select">
+                <option value="0">Not selected</option>
+                <option v-for="item in categories" v-bind:value="{ id: item.id, text: item.name }">
+                    {{ item.name }}
+                </option>
+            </select>
+        </div>
+        <div class="col-sm-8">
+            <span v-for="item in selectedCategories" class="badge text-bg-info" style="margin-right: 8px;">
+                {{ item.text}}
+                <i style="color: red;" @click="removeCategory(item)">X</i></span>
+        </div>
     </div>
-
-    <div>
-        <select @change="onCountryChange($event)" v-model="selectedCountry">
-            <option value="0">Not selected</option>
-            <option v-for="item in countries" v-bind:value="{ id: item.id, text: item.name }">
-                {{ item.name }}
-            </option>
-        </select>
+    <div class="mb-3 row">
+        <label for="lstCountries" class="form-label">Категория</label>
+        <div class="col-sm-3">
+            <select id="lstCountries" @change="onCountryChange($event)" v-model="selectedCountry" class="form-select">
+                <option value="0">Not selected</option>
+                <option v-for="item in countries" v-bind:value="{ id: item.id, text: item.name }">
+                    {{ item.name }}
+                </option>
+            </select>
+        </div>
+        <div class="col-sm-8">
+            <span v-for="item in selectedCountries" class="badge text-bg-info" style="margin-right: 8px;">
+                {{ item.text}}
+                <i style="color: red;" @click="removeCountry(item)">X</i></span>
+        </div>
     </div>
-
     <table class="table table-bordered">
         <thead>
             <tr>
@@ -61,6 +76,7 @@ export default {
     },
     methods: {
         getItems() {
+            if (this.selectedCountries.length == 0) return;
             axios
                 .get(`api/events?categoryIds=${this.categoryIds}&countryIds=${this.countryIds}`)
                 .then(res => {
@@ -91,6 +107,7 @@ export default {
                 });
         },
         onCategoryChange() {
+            if (!this.selectedCategory.id) return;
             if (this.selectedCategories.filter(e => e.id === this.selectedCategory.id).length == 0) {
                 this.selectedCategories.push(this.selectedCategory);
             }
@@ -99,10 +116,29 @@ export default {
             this.getItems();
         },
         onCountryChange() {
+            if (!this.selectedCountry.id) return;
             if (this.selectedCountries.filter(e => e.id === this.selectedCountry.id).length == 0) {
                 this.selectedCountries.push(this.selectedCountry);
             }
             this.countryIds = this.selectedCountries.map(i => i.id).join(',');
+            this.getItems();
+        },
+        removeCategory(item) {
+            var index = this.selectedCategories.indexOf(item);
+            if (index > -1) {
+                this.selectedCategories.splice(index, 1);
+            }
+            this.categoryIds = this.selectedCategories.map(i => i.id).join(',');
+            if (this.selectedCountries.length == 0) this.events = [];
+            this.getItems();
+        },
+        removeCountry(item) {
+            var index = this.selectedCountries.indexOf(item);
+            if (index > -1) {
+                this.selectedCountries.splice(index, 1);
+            }
+            this.countryIds = this.selectedCountries.map(i => i.id).join(',');
+            if (this.selectedCountries.length == 0) this.events = [];
             this.getItems();
         }
     }
